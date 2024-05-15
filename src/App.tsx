df45b94f3a867cd00c3e114a63581d235b96fc9f
Hrynchuk Taras <hrynchuktt@gmail.com>
2024-05-15 15:14:40 +0300

    base map and markers implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,85 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import {  useState } from "react";
+import Map, { MapLayerMouseEvent, Marker, MarkerDragEvent, ViewState } from "react-map-gl";
+import "mapbox-gl/dist/mapbox-gl.css"; 
+
+const ACCESS_TOKEN = import.meta.env.VITE_MAPBOX_ACCESS_TOKEN; 
+
+interface IViewState extends ViewState {
+   latitude: number;
+   longitude: number;
+   zoom: number;
+ }
+ 
+ interface IMarker {
+   id: number;
+   longitude: number;
+   latitude: number;
+ }
 
 function App() {
-  const [count, setCount] = useState(0)
+   const [viewState, setViewState] = useState<Partial<IViewState>>({
+      longitude: 24.031111,
+      latitude: 49.842957,
+      zoom: 14,
+   }); 
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+   
+
+   const [markers, setMarkers] = useState<IMarker[]>([]);
+
+   const addMarker = (e: MapLayerMouseEvent) => {
+     const newMarker = {
+       id: markers.length,
+       longitude: e.lngLat.lng,
+       latitude: e.lngLat.lat,
+     };
+     setMarkers([...markers, newMarker]);
+   };
+ 
+   const deleteMarker = (id: number, event: React.MouseEvent) => {
+      event.stopPropagation();
+     setMarkers(markers.filter((marker) => marker.id !== id));
+   };
+ 
+   const onMarkerDragEnd = (e: MarkerDragEvent, id: number) => {
+     setMarkers(
+         markers.map((marker) =>
+            marker.id === id
+            ? { ...marker, longitude: e.lngLat.lng, latitude: e.lngLat.lat }
+            : marker
+         )
+     );
+   };
+
+   return (
+      <div style={{ width: "100%" }}>
+      <Map
+        mapboxAccessToken={ACCESS_TOKEN}
+        {...viewState}
+        onMove={(evt) => setViewState(evt.viewState)}
+        style={{ width: "100%", height: 800 }}
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+        onClick={addMarker}
+      >
+        {markers.map((marker) => (
+          <Marker
+            key={marker.id}
+            draggable
+            longitude={marker.longitude}
+            latitude={marker.latitude}
+            anchor="bottom"
+            onDragEnd={(event) => onMarkerDragEnd(event, marker.id)}
+          >
+            <div onClick={(e) => deleteMarker(marker.id, e)}>
+              <img width={40} src="/marker.svg" />
+              <span style={{ position: 'absolute', top: '12%', left: '40%', color: 'white', fontWeight: 'bold' }}>{marker.id + 1}</span>
+            </div>
+          </Marker>
+        ))}
+      </Map>
+    </div>
+   );
 }
 
-export default App
+export default App;
